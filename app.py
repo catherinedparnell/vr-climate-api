6090cc8ef1be6fc652a41cb7b1133b7a53a88314
Catherine Parnell <catherine.d.parnell.22@dartmouth.edu>
2022-02-27 22:28:00 -0500

    csv and commenting structure

--- app.py
+++ app.py
@@ -4,3 +4,17 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello world, this is my api'
+
+
+# CSV:
+# 1. temperature
+# 2. precipitation
+# 3. sea-level
+
+# load in csv as pandas dataframe
+
+# tags that correspond to SSP
+
+# df[0][year] -> would give us theoretically the mean temperature
+
+# return jsonify({ temp: temp, precip: precip, sealevel: sealevel })
